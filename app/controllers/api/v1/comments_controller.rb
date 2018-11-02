ff48bf0561451f7ada702a6908f4e38df759158b
Hamid Khosravian <hamid.kh.1991@gmail.com>
2018-11-02 23:28:44 +0330

    add base of comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,18 +6,48 @@ module Api
       before_action :find_comment, except: [:index, :create]
 
       def index
+        @comments = Comment.all
+        render "api/v1/comments/index", status: :ok
       end
 
       def show
+        render "api/v1/comments/show", status: :ok
       end
 
       def create
+        result = Comments::Save.call(
+          title: params[:title],
+          body: params[:body],
+          article: @article,
+          user: @current_user
+        );
+
+        raise BadRequestError, result.errors if result.failure?
+
+        @comment = result.comment
+        render "api/v1/comments/show", status: :created
       end
 
       def update
+        result = Comments::Update.call(
+          title: params[:title],
+          body: params[:body],
+          comment: @comment,
+          article: @article,
+          user: @current_user
+        );
+
+        raise BadRequestError, result.errors if result.failure?
+
+        @comment = result.comment
+        authorize @article
+
+        render "api/v1/comments/show", status: :ok
       end
 
       def destroy
+        authorize @comment
+        @comment.destroy!
       end
 
       private
