0ccfa9180dbc27a79ed58c3d95e430ed6795b690
Hamid Khosravian <hamid.kh.1991@gmail.com>
2018-11-04 23:05:31 +0330

    change to puma

--- config/containers/app/server.sh
+++ config/containers/app/server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
-exec bundle exec unicorn -c /var/www/blog_sample/config/containers/app/unicorn.rb -E $RAILS_ENV;
+RACK_ENV=$RAILS_ENV bundle exec puma -p 8080
